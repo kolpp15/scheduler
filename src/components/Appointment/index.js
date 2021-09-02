54a32572f0ad81907351182940338280dc0e6ec8
Brian Sohn <kolpp15@gmail.com>
2021-09-01 18:21:39 -0700

    creaded Appointment index and header

---
+++ src/components/Appointment/index.js
@@ -0,0 +1,12 @@
+import React from "react";
+import classNames from 'classnames';
+import "components/Appointment/styles.scss";
+import Header from "./Header";
+
+export default function Appointment(props) {
+
+
+  return (
+    <article className="appointment"></article>
+  )
+}
--- stories/index.js
+++ stories/index.js
@@ -10,6 +10,8 @@ import DayListItem from "components/DayListItem";
 import DayList from "components/DayList";
 import InterviewerListItem from "components/InterviewerListItem";
 import InterviewerList from "components/InterviewerList";
+import Appointment from "components/Appointment/index";
+import Header from "components/Appointment/Header";
 
 // Button Storybook ------------------------------------------------------
 storiesOf("Button", module)
@@ -136,5 +138,12 @@ storiesOf("InterviewerList", module)
     />
   ));
 
-
+// Appointment Story ------------------------------------------------------
+storiesOf("Appointment", module)
+    .addParameters({
+      backgrounds: [{ name: "white", value: "#FFF", default: true}]
+    })
+    .add("Appointment", () => <Appointment />)
+    .add("Appointment with Time", () => <Appointment time="12pm" />)
+    .add("Header", () => <Header time="12pm" />)
 
