6ebf4c003e33b3ad64bd6e9a7bd21bebacec8f8b
Brian Sohn <kolpp15@gmail.com>
2021-09-01 19:10:00 -0700

    delete confirmation messageL onCancel and onConfirm added

--- stories/index.js
+++ stories/index.js
@@ -5,15 +5,16 @@ import { action } from "@storybook/addon-actions";
 
 import "index.scss";
 
-import Button from "components/Button";
-import DayListItem from "components/DayListItem";
-import DayList from "components/DayList";
+import Button from              "components/Button";
+import DayListItem from         "components/DayListItem";
+import DayList from             "components/DayList";
 import InterviewerListItem from "components/InterviewerListItem";
-import InterviewerList from "components/InterviewerList";
-import Appointment from "components/Appointment/index";
-import Header from "components/Appointment/Header";
-import Empty from "components/Appointment/Empty";
-import Show from "components/Appointment/Show";
+import InterviewerList from     "components/InterviewerList";
+import Appointment from         "components/Appointment/index";
+import Header from              "components/Appointment/Header";
+import Empty from               "components/Appointment/Empty";
+import Show from                "components/Appointment/Show";
+import Confirm from             "components/Appointment/Confirm";
 
 // Button Storybook ------------------------------------------------------
 storiesOf("Button", module)
@@ -150,3 +151,6 @@ storiesOf("Appointment", module)
     .add("Header", () => <Header time="12pm" />)
     .add("Empty", () => <Empty onAdd={action("onAdd")} />)
     .add("Show", () => <Show student="Lydia Miller-Jones" interviewer={interviewers[0].name} onEdit={action("onEdit")} onDelete={action("onDelete")}/>)
+    .add("Confirm", () => <Confirm onCancel={action("onCancel")} onConfirm={action("onConfirm")}/>)
+
+
