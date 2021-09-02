211de17d6ba76ca2097aa41189ee02a4571decf8
Brian Sohn <kolpp15@gmail.com>
2021-09-01 19:01:51 -0700

    added Show component that has onEdit, onDelete function onClick

--- stories/index.js
+++ stories/index.js
@@ -12,6 +12,8 @@ import InterviewerListItem from "components/InterviewerListItem";
 import InterviewerList from "components/InterviewerList";
 import Appointment from "components/Appointment/index";
 import Header from "components/Appointment/Header";
+import Empty from "components/Appointment/Empty";
+import Show from "components/Appointment/Show";
 
 // Button Storybook ------------------------------------------------------
 storiesOf("Button", module)
@@ -146,4 +148,5 @@ storiesOf("Appointment", module)
     .add("Appointment", () => <Appointment />)
     .add("Appointment with Time", () => <Appointment time="12pm" />)
     .add("Header", () => <Header time="12pm" />)
-
+    .add("Empty", () => <Empty onAdd={action("onAdd")} />)
+    .add("Show", () => <Show student="Lydia Miller-Jones" interviewer={interviewers[0].name} onEdit={action("onEdit")} onDelete={action("onDelete")}/>)
