7c030c38d38caf1bc7bc4efe196695195b957057
Brian Sohn <kolpp15@gmail.com>
2021-09-01 22:14:14 -0700

    updating Appointment component with ternary operator Show or Empty

--- src/components/Appointment/index.js
+++ src/components/Appointment/index.js
@@ -1,12 +1,20 @@
-import React from "react";
+import React, { Fragment } from "react";
 import classNames from 'classnames';
 import "components/Appointment/styles.scss";
 import Header from "./Header";
+import Show from "./Show";
+import Empty from "./Empty";
 
 export default function Appointment(props) {
 
 
   return (
-    <article className="appointment"></article>
+    <article className="appointment">
+      <Header time={props.time} />
+
+      {props.interview ? <Show /> : <Empty />}
+      
+
+    </article>
   )
 }
--- stories/index.js
+++ stories/index.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { Fragment } from "react";
 
 import { storiesOf } from "@storybook/react";
 import { action } from "@storybook/addon-actions";
@@ -160,5 +160,9 @@ storiesOf("Appointment", module)
     .add("Error", () => <Error  message="Could Not Delete Appointment" onClose={action("onClose")}/>)
     .add("Edit", () => <Form name="Student's Name" interviewers={interviewers} interviewer={3} onSave={action("onSave")} onCancel={action("onCancel")}/>)
     .add("Create", () => <Form interviewers={interviewers} onSave={action("onSave")} onCancel={action("onCancel")}/>)
-
-
+    .add("Appointment Empty", () => (
+      <Fragment>
+        <Appointment id={1} time="12pm" />
+        <Appointment id="last" time="1pm" />
+      </Fragment>
+    ))
